2556a77cd60d1ce55dc2c712fb4826dc385ac354
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2023-11-13 00:46:42 +0600

    Root api is added

--- paths/routes/root.js
+++ paths/routes/root.js
@@ -1,19 +1,16 @@
-const swaggerUi = require('swagger-ui-express');
 const express = require("express");
-const appDocument = require('../../docs.json');
-const swaggerDocument = require('../../swagger.json');
-const app = express();
-// app.get("/api/v1/api-docs/swagger.json", (req, res) => res.json(swaggerDocument));
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(appDocument));
-// app.use('/', swaggerUi.serve, swaggerUi.setup(appDocument));
+// var fs = require('fs');
+// const root = require("../../api/views/root/index.html");
 const router = express.Router();
 // Just simple info for root api endpoint
-// router.get("/", (req, res) => {
-//     res.sendFile
-// });
-
-// router.get("/api-docs", swaggerUi.serve, swaggerUi.setup(appDocument));
-
-// router.get("/api/v1/api-docs/swagger.json", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.get("/", (_, res) => {
+    res.json({
+        message: "Welcome to the Financial Health Score API",
+        server: process.env.NODE_ENV === "development"
+            ? "http://localhost:8000/api-docs"
+            : "https://financial-health-score-server.onrender.com/api-docs",
+        version: "1.0.0",
+    });
+});
 
 module.exports = router;
