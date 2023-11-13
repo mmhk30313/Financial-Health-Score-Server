82793bb72949a1254d5f44db91a2363887617b7e
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2023-11-13 19:06:06 +0600

    Updated for cyclic issues

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const fileUpload = require("express-fileupload");
 const app = express();
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const { authenticateJWT } = require("../api/controllers/auth/index.js");
-require("../configs/env.config.js");
-require("../configs/db.config.js");
+const { authenticateJWT } = require("./api/controllers/auth/index.js");
+require("./configs/env.config.js");
+require("./configs/db.config.js");
 const swaggerUi = require('swagger-ui-express');
 const cookieParser = require("cookie-parser");
 const router = express.Router();
@@ -24,8 +24,8 @@ app.use("/.netlify/functions/index", router);
 app.use('/api/static', express.static('uploads'));
 
 // middleware that is specific to this router
-const appDocument = require('./docs.json');
-const swaggerDocument = require('./swagger.json');
+const appDocument = require('./swagger/docs.json');
+const swaggerDocument = require('./swagger/swagger.json');
 // app.use('/swagger-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(appDocument));
 
@@ -52,7 +52,7 @@ app.use(async (req, res, next) => {
 // console.log({paths});
 
 // auth middleware
-require("../paths/route_path.js")(app);
+require("./paths/route_path.js")(app);
 
 // route_paths?.map(route_path => app.use('/api', route_path));
 
@@ -61,9 +61,9 @@ global.appRoot = path.resolve(__dirname);
 
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log("\x1b[32m%s\x1b[0m", "Node server started at", process.env.PROD_URL ? process.env.PROD_URL : `http://localhost:${process.env.PORT || 8000}`);
-    console.log("\x1b[31m%s\x1b[0m", "Node server started at", process.env.PROD_URL ? process.env.PROD_URL + "/api-docs" : `http://localhost:${process.env.PORT || 8000}/api-docs`);
-    console.log("\x1b[30m%s\x1b[0m", "Node server started at", process.env.PROD_URL ? process.env.PROD_URL + "/api/v1/api-docs/swagger.json" : `http://localhost:${process.env.PORT || 8000}/api/v1/api-docs/swagger.json`);
+    console.log("\x1b[32m%s\x1b[0m", "Node server started at", process.env.NODE_ENV === "production" ? process.env.PROD_URL : `http://localhost:${process.env.PORT || 8000}`);
+    console.log("\x1b[31m%s\x1b[0m", "Node server started at", process.env.NODE_ENV === "production" ? process.env.PROD_URL + "/api-docs" : `http://localhost:${process.env.PORT || 8000}/api-docs`);
+    console.log("\x1b[30m%s\x1b[0m", "Node server started at", process.env.NODE_ENV === "production" ? process.env.PROD_URL + "/api/v1/api-docs/swagger.json" : `http://localhost:${process.env.PORT || 8000}/api/v1/api-docs/swagger.json`);
 });
 
 module.exports.handler = serverless(app);
